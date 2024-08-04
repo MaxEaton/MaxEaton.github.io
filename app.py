753c234c466b1714e05e90a6ed0161e22fdc588a
MaxEaton <maximiliano.k.eaton@gmail.com>
2024-08-04 16:17:59 -0600

    Added FCT project

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
+from fct import fct_blueprint
 
 app = Flask(__name__)
+app.register_blueprint(fct_blueprint, url_prefix='/fct')
 
 @app.route('/')
 def home():
